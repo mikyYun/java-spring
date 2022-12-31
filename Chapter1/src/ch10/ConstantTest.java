54c0e68a175c7d49891b8d96de039e6ddc6316b6
mikyYun <mkyun2714@gmail.com>
2022-12-30 20:36:14 -0800

    logicalTest

--- Chapter1/src/ch10/ConstantTest.java
+++ Chapter1/src/ch10/ConstantTest.java
@@ -9,12 +9,22 @@ public class ConstantTest {
 		
 		MIN_NUM = 0;
 		
-		System.out.println(MAX_NUM);
+		System.out.println(MAX_NUM + " And " + MIN_NUM);
 		System.out.println(MIN_NUM);
 
 //		MIN_NUM = 1; => cannot reassign to constant variable
 //		System.out.println(MIN_NUM);
 
+	
+		double dNum = 1.2;
+		float fNum = 0.9F;
+		
+		int iNum1 = (int)dNum + (int)fNum; // 1 + 0
+		int iNum2 = (int)(dNum + fNum); // int(2.1)
+		
+		System.out.println(iNum1);
+		System.out.println(iNum2);
+		
 		
 	}
 
