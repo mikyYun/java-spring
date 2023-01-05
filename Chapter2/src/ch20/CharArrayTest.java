881230be26c90618d0faf756acb4f8d56356210e
mikyYun <mkyun2714@gmail.com>
2023-01-05 06:20:08 -0800

    array and test

--- Chapter2/src/ch20/CharArrayTest.java
+++ Chapter2/src/ch20/CharArrayTest.java
@@ -3,7 +3,7 @@ package ch20;
 public class CharArrayTest {
 
 	public static void main(String[] args) {
-
+		// array with 26 empty elements
 		char[] alphs = new char[26];
 		char ch = 'A';
 		System.out.println(alphs.length);
