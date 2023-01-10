05ab2e942c6974cfff81f25d35e93c95ff27ab99
mikyYun <mkyun2714@gmail.com>
2023-01-10 12:29:28 -0800

    fundamental

--- javajungsuk_basic-master/javajungsuk_basic_src/ch01/src/Hello.java
+++ javajungsuk_basic-master/javajungsuk_basic_src/ch01/src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world");
+		System.out.print("This print contents and not change line");
+		System.out.println("//// still on a same line");
+		System.out.println("new line");
+		
 	}
 }
